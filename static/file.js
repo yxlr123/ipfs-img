52de45a965fa46ece6902508ce23ce9e3758bcf9
幽香乐容 <109068213+yxlr123@users.noreply.github.com>
2023-01-03 19:30:31 +0800

    Update file.js

--- static/file.js
+++ static/file.js
@@ -58,16 +58,16 @@ $(() => {
  	        var file = files[i];
  	        if(!file){return false;}
  	        var fileSize = file.size;
-            var maxSize = 5242880*20;
+            var maxSize = 5242880*200000000;
             if(ext !='.PNG' && ext !='.GIF' && ext !='.JPG' && ext !='.JPEG' && ext !='.BMP'&& ext !='.SVG'&& ext !='.TIF'&& ext !='.ICO'&& ext !='.3GP'&& ext !='.AVI'&& ext !='.FLV'&& ext !='.MOV'&& ext !='.MKV'&& ext !='.MP4'&& ext !='.MP3'&& ext !='.FLAC'&& ext !='.WEBP'&& ext !='.MPG'&& ext !='.MPEG'&& ext !='.M3U8'&& ext !='.RM'&& ext !='.RMVB'&& ext !='.TS'&& ext !='.wmv'&& ext !='.asf'&& ext !='.webm'&& ext !='.ogg'&& ext !='.ACC'&& ext !='.M4A'&& ext !='.APE'&& ext !='.TXT'&& ext !='.JSON'&& ext !='.DOC'&& ext !='.DOCX'&& ext !='.XLS'&& ext !='.XLSX'&& ext !='.CSV'&& ext !='.SQL'&& ext !='.BAK'&& ext !='.PPT'&& ext !='.PPTX'&& ext !='.ZIP'&& ext !='.RAR'&& ext !='.7Z'&& ext !='.GZ'&& ext !='.TAR'&& ext !='.ISO'&& ext !='.MD'&& ext !='.SH'){  
  		        parent.alert('文件类型错误,请上传图片类型');
  		        $('#file').val(null);return false;  
  	        }else if(parseInt(fileSize) >= parseInt(maxSize)){  
- 		        parent.alert('上传的文件不能超过'+maxSize/1024/1024+'MB');return false;  
+ 		        parent.alert('上传的文件不能超过'+maxSize/5/5+'GB');return false;  
  	        }else{
  	        document.querySelector('.container').classList.add('start')
  	        var type = $('#id');
-  	        api = 'https://ipfs.infura.io:5001/api/v0/add?pin=true'
+  	        api = 'https://ipfs.staging.infura.org:5001/api/v0/add?pin=true'
             let formData = new FormData();
             formData.append('file', files[i]);
             let randomClass = Date.now().toString(36);
